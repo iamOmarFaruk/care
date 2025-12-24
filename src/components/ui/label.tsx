6b4a3250082e744370e8ac831feba7c4bf9c8e57
Omar Faruk <omarbg.bd@gmail.com>
2025-12-24 15:37:06 +0600

    style: Increase label bottom margin for improved spacing.

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -7,7 +7,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const labelVariants = cva(
-    "block text-sm font-medium leading-none mb-2 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+    "block text-sm font-medium leading-none mb-3 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 )
 
 const Label = React.forwardRef<
